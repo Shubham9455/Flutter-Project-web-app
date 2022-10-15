b6d3c7d38cea8886b9f9a120e6a098cc1d98cc79
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-10-15 10:57:09 +0530

    changes in api and alt img

--- src/componants/service/Data.js
+++ src/componants/service/Data.js
@@ -2,73 +2,69 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
-const api_key = "b442ebb0e33946faa86665f2a136d83a";
 
-const url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=" + api_key;
+const api_key = "pub_12307fb7955b1e1a3bcc5624d403e8b65582f";
 
-export const data = () => {
-  return <div>data</div>;
-};
+const url = "https://newsdata.io/api/1/news?apikey=" + api_key + "&country=in";
 
 const Data = () => {
   const [data, setData] = useState([]);
   const getData = async () => {
     const response = await fetch(url);
-    const data = await response.json();
-    setData(data.articles);
-    console.log(data.articles);
+    const mydata = await response.json();
+    console.log("hello");
+    console.log(mydata.results);
+    setData(mydata.results);
+    console.log(mydata.results);
   };
+
   useEffect(() => {
     getData();
   }, []);
   return (
-    <div style={{margin:"10px 20%"}}>
+    <div style={{ margin: "10px 20%" }}>
       <h1>News</h1>
-      {data?data.map((item,index) => {
-        if(item.urlToImage)
-        return (
-          <div className="news" id = {index}>
-            <Card
-              style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(3,1fr)",
-                gridGap: "2rem",
-                gridAutoRows: "auto",
-                margin: "20px"
-                
-              }}
-            >
-              <Card.Img style={{borderRadius:"8px" ,display:"block"}} variant="top" src={item.urlToImage} />
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                  {item.description}
-                </Card.Text>
-                <Button variant="primary" href={item.url}>Visit Site</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        );
-      }):<h1>Loading</h1>}
+      {data ? (
+        data.map((item, index) => {
+          if (1)
+            return (
+              <div className="news" id={index}>
+                <Card
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(3,1fr)",
+                    gridGap: "2rem",
+                    gridAutoRows: "auto",
+                    margin: "20px",
+                  }}
+                >
+                  <Card.Img
+                    style={{
+                      borderRadius: "8px",
+                      display: "block",
+                      height: "15rem",
+                    }}
+                    variant="top"
+                    src={
+                      item.image_url ? item.image_url : "/icons8-news-480.png"
+                    }
+                  />
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{/* {item.description} */}</Card.Text>
+                    <Button variant="primary" href={item.link}>
+                      Visit Site
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+        })
+      ) : (
+        <h1>Loading</h1>
+      )}
     </div>
   );
 };
 
 export default Data;
-
-// <Card
-//   style={{
-//     display: "grid",
-//     gridTemplateColumns: "repeat(3,1fr)",
-//     gridGap: "2rem",
-//     gridAutoRows: "auto",
-//     margin: "20px",
-//   }}
-// >
-//   <Card.Img variant="top" src={item.urlToImage} />
-//   <Card.Body>
-//     <Card.Text>
-//       <h3>{item.title}</h3>
-//     </Card.Text>
-//   </Card.Body>
-// </Card>;
